277a804747a62c913c4b1c7862530714ae808c28
muschellij2 <muschellij2@gmail.com>
2018-04-27 10:13:04 -0400

    fixing tests

--- tests/testthat/test-image_df.R
+++ tests/testthat/test-image_df.R
@@ -1,6 +1,6 @@
 test_that("Data is installed", {
 
-  if ("sri24" %in% installed.packages()) {
+  if ("sri24" %in% installed.packages()[, "Package"]) {
     expect_is(sri24_image_df(), "data.frame")
   } else {
     expect_is(sri24_image_df(), "NULL")
